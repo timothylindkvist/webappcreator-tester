d74b64ba80bc481dc6f7dcc9e869975d2a504574
timothylindkvist <timothy.lindkvist@gmail.com>
2025-08-21 15:04:08 +0200

    a

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,11 @@
 import Builder from '@/components/Builder'
 import ChatWidget from '@/components/ChatWidget'
 import { BuilderProvider } from '@/components/builder-context'
-import StreamChatDemo from '@/components/StreamChatDemo'
 
 export default function Page() {
   return (
     <BuilderProvider>
-      <main className="p-6 space-y-10">
-        {/* Streaming demo at the top */}
-        <section>
-          <StreamChatDemo />
-        </section>
-
-        {/* Your builder + chat UI */}
+      <main className="p-6">
         <div className="grid grid-cols-1 lg:grid-cols-[1fr_360px] gap-6">
           <div className="min-h-[70vh]">
             <Builder />
